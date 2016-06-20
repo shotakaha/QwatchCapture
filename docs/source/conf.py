fd369ecec27cb4b3584decfff5d416caaf591fce
Shota TAKAHASHI <shotakaha@gmail.com>
2016-06-21 08:53:38 +0900

    Replace QwatchCapture with KumaWatch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# QwatchCapture documentation build configuration file, created by
+# KumaWatch documentation build configuration file, created by
 # sphinx-quickstart on Thu Sep 24 15:54:21 2015.
 #
 # This file is execfile()d with the current directory set to its
@@ -56,7 +56,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'QwatchCapture'
+project = 'KumaWatch'
 copyright = '2015, Shota Takahashi'
 author = 'Shota Takahashi'
 
@@ -210,7 +210,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'QwatchCaptureDoc'
+htmlhelp_basename = 'KumaWatchDoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -232,7 +232,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'QwatchCapture.tex', 'QwatchCapture Documentation',
+  (master_doc, 'KumaWatch.tex', 'KumaWatch Documentation',
    'Shota Takahashi', 'howto'),
 ]
 
@@ -262,7 +262,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'qwatchcapture', 'QwatchCapture Documentation',
+    (master_doc, 'KumaWatch', 'KumaWatch Documentation',
      [author], 1)
 ]
 
@@ -276,8 +276,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'QwatchCapture', 'QwatchCapture Documentation',
-   author, 'QwatchCapture', 'One line description of project.',
+  (master_doc, 'KumaWatch', 'KumaWatch Documentation',
+   author, 'KumaWatch', 'One line description of project.',
    'Miscellaneous'),
 ]
 
